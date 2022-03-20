17d0ed41e90ce9270ad07f7c18bd429b2b5e85af
Jihyun Kim <ang21icjh@gmail.com>
2022-03-21 08:31:59 +0900

    feat: modal about this project / rename: css files

--- src/more.js
+++ src/more.js
@@ -1,36 +1,26 @@
-// Reset
-document.querySelector(".resetBtn").addEventListener("click", () => {
-  if (confirm("데이터를 되돌릴 수 없어요! 그대로 진행할까요?")) {
-    localStorage.clear();
-    location.reload();
-  }
-});
-
-// Modal
-
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
-const openBtns = document.querySelectorAll(".show-modal");
+const openBtn = document.querySelector(".show-modal");
 const closeBtn = document.querySelector(".close-modal");
 
-const HIDDEN_KEY = "hidden";
-
 function openModal() {
-  modal.classList.remove(HIDDEN_KEY);
-  overlay.classList.remove(HIDDEN_KEY);
+  modal.classList.remove(CLASSES.hidden);
+  overlay.classList.remove(CLASSES.hidden);
 }
 
 function closeModal() {
-  modal.classList.add(HIDDEN_KEY);
-  overlay.classList.add(HIDDEN_KEY);
+  modal.classList.add(CLASSES.hidden);
+  overlay.classList.add(CLASSES.hidden);
 }
 
-for (const btn of openBtns) {
-  btn.addEventListener("click", openModal);
-}
-
+openBtn.addEventListener("click", openModal);
 closeBtn.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
-document.addEventListener("keydown", (e) => {
-  if (e.key === "Escape" && !modal.classList.contains(HIDDEN_KEY)) closeModal();
+
+// Reset
+modal.querySelector(".resetBtn").addEventListener("click", () => {
+  if (confirm("데이터를 되돌릴 수 없어요! 그래도 진행할까요?")) {
+    localStorage.clear();
+    location.reload();
+  }
 });
